5957994e4d8a1e371a023d3d489dffac88b27f5b
avgmensch <git-push@judex.eu>
2023-12-02 18:25:05 +0100

    add missing test

--- test/day1.test.js
+++ test/day1.test.js
@@ -8,3 +8,9 @@ Deno.test("pqr3stu8vwx", () => assertEquals(aocDay1("pqr3stu8vwx"), 38));
 Deno.test("a1b2c3d4e5f", () => assertEquals(aocDay1("a1b2c3d4e5f"), 15));
 
 Deno.test("treb7uchet", () => assertEquals(aocDay1("treb7uchet"), 77));
+
+Deno.test("sum up solutions", () =>
+  assertEquals(
+    aocDay1("1abc2") + aocDay1("pqr3stu8vwx") + aocDay1("a1b2c3d4e5f") + aocDay1("treb7uchet"),
+    142,
+  ));
