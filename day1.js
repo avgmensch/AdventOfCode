fa1df20198249ce4c0fe1b95a0e9664f077af485
avgmensch <git-push@judex.eu>
2023-12-02 18:24:53 +0100

    refactor jsdoc

--- day1.js
+++ day1.js
@@ -1,15 +1,7 @@
 // @ts-check
 
 /**
- * # Day 1
- *
- * The newly-improved calibration document consists of lines of text; each line originally
- * contained a specific calibration value that the Elves now need to recover. On each line, the
- * calibration value can be found by combining the first digit and the last digit (in that order)
- * to form a single two-digit number.
- *
  * https://adventofcode.com/2023/day/1
- *
  * @param {string} str
  * @returns {number}
  */
